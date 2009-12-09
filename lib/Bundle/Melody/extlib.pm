9b9d37758d24b67238195510223412029bcb05c4
Timothy Appnel <tim@appnel.com>
2009-12-09 16:20:32 -0500

    Reformatted CONTENTS section. Added Cache version number. Added footnote and description about package subsets in some distros of Melody.

--- lib/Bundle/Melody/extlib.pm
+++ lib/Bundle/Melody/extlib.pm
@@ -4,7 +4,7 @@ use v5.8.1;
 use strict;
 
 use vars qw($VERSION);
-$VERSION = '0.9.6a';
+$VERSION = '0.9.6b';
 
 1;
 
@@ -30,59 +30,115 @@ C<perl -MCPAN -e "install Bundle::Melody::extlib">
 
 =head1 CONTENTS
 
- Archive::Extract 0.08
- Attribute::Params::Validate 1.7
- bignum 0.23 (subset)
- Cache (subset)
- CGI 3.45
- Class::Accessor 0.22
- Class::Data::Inheritable 0.06
- Class::ErrorHandler 0.01
- Class::Trigger 0.1001
- Crypt::DH 0.06
- Data::ObjectDriver 0.06
- File::Copy::Recursive 0.23
- File::Temp 0.12
- Heap::Fibonacci 0.71
- HTML::Template 2.8
- I18N::LangTags 0.35
- Image::Size 2.93
- IO::Scalar 2.110 (subset) 
- IPC::Cmd 0.24
- Jcode 0.88
- JSON 2.12
- Log::Log4perl
- Log::Dispath
- Locale::Maketext 1.13
- Lucene::QueryParser 1.04
- LWP 5.831
- Math::BigInt 1.63
- MIME::Charset 0.044
- MIME::EncWords 0.040
- Module::Load 0.10
- Module::Load::Conditional 0.08
- Net::OAuth 0.11
- Net::OpenID::Consumer 1.03
- Params::Check 0.24
- Params::Validate 0.73
- Sub::Install
- SOAP::Lite 0.710.08
- TheSchwartz 1.07 
- UNIVERSAL 0.11
- URI 1.36
- URI::Fetch 0.08
- version 0.76
- XML::Elemental 2.1
- XML::NameSpaceSupport 1.09
- XML::SAX 0.96
- XML::Simple 2.14
- YAML::Tiny 1.12
+Archive::Extract 0.08
+
+Attribute::Params::Validate 1.7
+
+bignum 0.23 [1]
+
+Cache 2.04 [1]
+
+CGI 3.45
+
+Class::Accessor 0.22
+
+Class::Data::Inheritable 0.06
+
+Class::ErrorHandler 0.01
+
+Class::Trigger 0.1001
+
+Crypt::DH 0.06
+
+Data::ObjectDriver 0.06
+
+File::Copy::Recursive 0.23
+
+File::Temp 0.12
+
+Heap::Fibonacci 0.71
+
+HTML::Template 2.8
+
+I18N::LangTags 0.35
+
+Image::Size 2.93
+
+IO::Scalar 2.110 [1]
+
+IPC::Cmd 0.24
+
+Jcode 0.88
+
+JSON 2.12
+
+Log::Log4perl
+
+Log::Dispatch
+
+Locale::Maketext 1.13
+
+Lucene::QueryParser 1.04
+
+LWP 5.831
+
+Math::BigInt 1.63
+
+MIME::Charset 0.044
+
+MIME::EncWords 0.040
+
+Module::Load 0.10
+
+Module::Load::Conditional 0.08
+
+Net::OAuth 0.11
+
+Net::OpenID::Consumer 1.03
+
+Params::Check 0.24
+
+Params::Validate 0.73
+
+Sub::Install
+
+SOAP::Lite 0.710.08
+
+TheSchwartz 1.07 
+
+UNIVERSAL 0.11
+
+URI 1.36
+
+URI::Fetch 0.08
+
+version 0.76
+
+XML::Elemental 2.1
+
+XML::NameSpaceSupport 1.09
+
+XML::SAX 0.96
+
+XML::Simple 2.14
+
+YAML::Tiny 1.12
  
 =head1 DESCRIPTION
 
 This bundle contains the prerequisite CPAN modules for
 running Melody E<lt>http://openmelody.org/E<gt> that is
 commonly shipped in its extlib directory.
+
+[1] Distributions of Melody that include this bundle's
+contents in the C<extlib> folder only come with a subset of
+these packages. This is a practice inherited from Movable
+Type to assist users who cannot install packages that
+require compilation. (Typically these users are usng FTP to
+install the software because of a lack of command line
+experience or (no) permissions.) These subsets are the pure
+Perl parts that Melody requires to operate. This excluded
+parts are not believed to effect Melody's normal operation.
 
 =head1 SEE ALSO
 
